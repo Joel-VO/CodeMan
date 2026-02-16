35312a18b9d9d626c9ac7c5510d6c4756a249a31
joeloommen <joelvo9694@gmail.com>
2026-02-17 01:41:47 +0530

    modified llm code to accept ollama response

--- LLM/coder_LLM.py
+++ LLM/coder_LLM.py
@@ -1,9 +1,31 @@
-import torch
 import ollama
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+class LlmResponse:
+    def __init__(self, sys_prompt, usr_prompt, model=""):
+        self.SYS_PROMPT = sys_prompt
+        self.USER_PROMPT = usr_prompt
+        self.model_name = model
 
-print(device) # debugging
+        try:
+            ollama.chat(self.model_name)
 
-def llm_response():
-    pass
+        except ollama.ResponseError as e:
+            print('Error:', e.error)
+            if e.status_code == 404:
+                try:
+                    ollama.pull(self.model_name)
+                except:
+                    print("Model pull failed, model doesn't exist in the ollama registery.")
+
+    def response():
+        response = ollama.chat(
+            self.model_name,
+            messages=[
+                {'role': 'system', 'content': self.SYS_PROMPT},
+                {'role': 'user', 'content': self.USER_PROMPT}
+            ]
+        )
+
+        print(response['message']['content']) # comment this out when testing, this is for debugging only
+        
+        return response['message']['content'] 
