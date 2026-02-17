c6c0b3249c27912fba864cb1ec0a8281eaf20997
joeloommen <joelvo9694@gmail.com>
2026-02-18 00:52:28 +0530

    llm improvement

--- LLM/coder_LLM.py
+++ LLM/coder_LLM.py
@@ -2,20 +2,33 @@ import ollama
 import re
 
 class LLMResponse:
-    def __init__(self, sys_prompt, model="qwen2.5-coder:7b"):
-        self.SYS_PROMPT = sys_prompt
+    def __init__(self, sys_prompt, rag_chunks, model="qwen2.5-coder:7b"):
+
+        self.SYS_PROMPT = f"""You are a professional coding agent. 
+        Below is relevant documentation that you may or may not use:
+        {rag_chunks}
+        """
+        
+        """Rag chunks have to be in format given below:
+        chunk1: [docs]
+        chunk2: [docs] etc
+        """
+
         self.model_name = model
 
         try:
+
             ollama.chat(self.model_name)
-            print("Loaded model")
+
         except ollama.ResponseError as e:
+
             print('Error:', e.error)
             if e.status_code == 404:
                 try:
                     ollama.pull(self.model_name)
                 except:
                     print("Model pull failed, model doesn't exist in the ollama registery.")
+
 
     def response(self, usr_prompt):
         response = ollama.chat(
@@ -26,7 +39,6 @@ class LLMResponse:
             ]
         )
 
-        # print(response['message']['content']) # comment this out when testing, this is for debugging only
         return response['message']['content'] 
 
     def get_code(self, response):
@@ -39,9 +51,10 @@ class LLMResponse:
         return [{"language": lang or None, "code": code.strip()} for lang, code in matches]
 
 
-# sys_prompt = "You are a profession coder"
 # user_prompt_out = "write the code for generating fibonacci series.user recursion"
 
 # model = LLMResponse(sys_prompt)
 # out = model.response(user_prompt_out)
-# model.get_code(out)
+# model_response_dict = model.get_code(out)
+
+# print(model_response_dict[0]['code']) ## this is the code to use when accessing just the code
